7c2c51116e6f9e3347e859d68af110b3ead077c5
Lancheng123 <thanongsak.khmap@gmail.com>
2022-12-19 15:30:37 +0700

    first commit

--- public/javascripts/data_wish.js
+++ public/javascripts/data_wish.js
@@ -25,21 +25,34 @@ function hideloader() {
 }
 // Function to define innerHTML for HTML table
 function show(data) {
-	let tab =
-		`<tr>
-        <th>เวลา</th>
-		<th>คำอวยพร</th>
-		<th>จาก</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	data.forEach((itemData) =>{
-		tab += `<tr>
-        <td>${ itemData.ประทับเวลา} </td>
-	<td>${ itemData.คำอวยพร} </td>
-	<td>${ itemData.ชื่อ} </td>
-</tr>`;
-	});
+	let tab = "";
+	data.sort((a, b) => {
+		return new Date(b.ประทับเวลา) - new Date(a.ประทับเวลา); // ascending
+	  })
+	// Loop to access all rows data.forEach((itemData)
+	for (let i = 0; i < 6; i++){
+		tab += `
+		<div class="col-md-4">
+		<div class="container" style="width:auto">
+        <div class="container roundded-none">
+          <p class="card-text">${ data[i].คำอวยพร}</p> 
+		  <h5 class="card-title d-none d-sm-block">${ data[i].ชื่อ}</h5>
+          <h6 class="card-subtitle mb-2 text-muted d-none d-sm-block">${ data[i].ประทับเวลา}</h6>
+		  <p class="card-text">__________________________</p>
+		  </div>
+		</div>
+      </div>`;
+
+
+
+
+      //<a href="#" class="tagc3" style="left: 136.828px; top: 34.752px; z-index: 102; opacity: 0.111076; color: rgb(255, 255, 255); padding: 5px; display: none;">${ itemData.คำอวยพร}</a>
+      //<a href="#" class="tagc3" style="left: 95.415px; top: 30.9076px; z-index: 103; opacity: 0.119797; color: rgb(255, 255, 255); padding: 5px; display: none;">${ itemData.คำอวยพร}</a>
+      //<a href="#" class="tagc1" style="left: 58.2577px; top: 45.3038px; z-index: 107; opacity: 0.181538; color: rgb(255, 255, 255); padding: 5px;">${ itemData.คำอวยพร}</a>
+      //<a href="#" class="tagc3" style="left: 65.4534px; top: 42.9294px; z-index: 119; opacity: 0.389813; color: rgb(255, 255, 255); padding: 5px;">${ itemData.คำอวยพร}</a>
+      //<a href="#" class="tagc2" style="left: 108.202px; top: 30.0199px; z-index: 102; opacity: 0.117901; color: rgb(255, 255, 255); padding: 5px; display: none;">${ itemData.คำอวยพร}</a>
+	//<a href="#" class="tagc1" style="left: 58.2577px; top: 45.3038px; z-index: 107; opacity: 0.181538; color: rgb(255, 255, 255); padding: 5px;">${ itemData.คำอวยพร}</a>`;
+	};
 	// Setting innerHTML as tab variable
-	document.getElementById("wish").innerHTML = tab;
+	document.getElementById("greetingcard").innerHTML = tab;
 }
