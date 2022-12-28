2a459e5443edabf7a30f03935b9c27b7d8b46655
Lancheng123 <thanongsak.khmap@gmail.com>
2022-12-28 12:25:49 +0700

    first commit

--- routes/history.js
+++ routes/history.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('history', { title: 'history' });
+  res.render('history', { title: 'ประวัติ' });
 });
 
 module.exports = router;
